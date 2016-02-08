f12f8fa5fcf55c68de52c80c56f3c9642568c150
Kenneth Shaw <kenneth.shaw@knq.io>
2016-02-08 10:38:02 +0700

    Adding pgtest.sh

---
+++ pgtest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+rm -rf p t
+mkdir -p p t
+
+go generate
+go build
+
+./xo pgsql://xodb:xodb@localhost/xodb -o p -s pg_catalog -C pgcatalog
+./xo pgsql://xodb:xodb@localhost/xodb -o t -s information_schema -C pgcatalog
+
+go build ./p/
+go build ./t/
