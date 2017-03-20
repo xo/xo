188a6dd3368423e9e96fac26bb93df63d08b5604
Kenneth Shaw <kenneth.shaw@knq.io>
2017-03-20 10:44:15 +0700

    Minor change to orstart.sh to not automatically expose port 8080

--- contrib/orstart.sh
+++ contrib/orstart.sh
@@ -11,9 +11,9 @@ docker stop $DNAME
 docker rm $DNAME
 
 # run instance
+#  -p 8080:8080 \
 docker run \
   -d \
-  -p 8080:8080 \
   -p 1521:1521 \
   -v /media/src/opt/oracle:/u01/app/oracle \
   -e DBCA_TOTAL_MEMORY=1024 \
