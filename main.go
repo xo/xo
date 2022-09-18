7191d101193680babcef384ec11f7fc30d535969
Kenneth Shaw <ken@usql.app>
2022-09-18 16:07:38 +0700

    Minor formatting in main.go

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 	"os"
 
 	// drivers
-	_ "github.com/microsoft/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/microsoft/go-mssqldb"
 	_ "github.com/sijms/go-ora/v2"
 
 	"github.com/xo/xo/cmd"
