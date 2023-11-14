eb9fcf995fad5b365ae333662d739e63664f1fa9
Kenneth Shaw <ken@usql.app>
2023-11-14 18:40:20 +0700

    Removing pgcatalog example

--- _examples/pgcatalog/pgtypes/query-gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xo query -o . -S model.xo.go -M -B -T PgType pg:// << END
-SELECT
-  DISTINCT typname::varchar AS name
-FROM pg_type
-WHERE typname NOT LIKE '\_%'
-  AND typname <> 'pg_type'
-END
